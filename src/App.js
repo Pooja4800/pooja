8f1c839a26769d96b22d38975684076d164d1eda
pooja4800 <poojam4800@gmail.com>
2022-08-16 12:02:13 +0530

    to my project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from "react"
-
+import React from "react";
 import "./App.css"
-import { Pooja } from "./screens/dashboard"
+import Login from "./screens/dashboard"
 
 export default function App() {
-    return (
+
+    return(
         <div className="App">
-            {/* <Dashboard/> */}
-            <Pooja
-                name={"Login"}
-                callBack={'handleclickHere'}
-                color={"red"}/>
-            <Pooja 
-                name={"Click Here"}
-                callBack={'handleclickHere'} 
-                color={"pink"}/>
-            <Pooja
-                name={"Sign Up"}
-                callBack={'handleclickHere'}
-                color={"green"}/>
+            <Login/>
         </div>
-    ) 
+    )
 }
--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -1,13 +1,79 @@
-import React from 'react';
-import './index.css'
+import React, { useState } from "react"
+import "./index.css";
+import {
+    TextField,
+    Button,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    InputAdornment,
+    IconButton,
+} from "@mui/material";
 
-export function Pooja(props) {
-    
+import{ Visibility, VisibilityOff } from "@mui/icons-material";
+
+const Login = () => {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[showPassword, setShowPassword] =useState(false);
+    const[errorType, setErrorType] = useState("");
+    const[errorMessage, setErrorMessage] = useState("");
+
+    const handlelogin = () => {
+        if (email == "") {
+            setErrorType("email");
+            setErrorMessage("Email Required");
+        } else if (password == ""){
+                setErrorType("pasword");
+                setErrorMessage("password Required"); 
+            } else {
+            alert(`${email}
+${password}`);
+        }
+    };
+
     return (
-        <div className='container'>
-            <div style={{borderRadius: 10,backgroundcolour: 'blue', width: 100,alignItems: 'center',height: 50 ,justifyContent:'center'}}>
-                <p style={{alignContent:'center',color:'blue'}}>{props.name}</p>
-            </div>
-        </div>      
-    )
-}
+        <div className="container">
+            <h1>Login</h1>
+                <TextField
+                    className="textField"
+                    label={"Email*"}
+                    type="email"
+                    value={email}
+                    variant="outlined"
+                    onChange={(e) => setEmail(e.target.value)}
+                    error={errorType === "email"}
+                    helperText={errorType === "email" ? errorMessage : ""}
+                />
+
+                <FormControl variant="outlined" className="textField" error={errorType==='password'}>
+                    <InputLabel htmlFor="Outlined-adorment-password">password*</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adorment-password"
+                        type={showPassword ? "text" : "password"}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        endAdorment= {
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="password"
+                    />
+                        {errorType==='passwor'&& <p style={{color:'red'}}>{errorMessage}</p>}
+                </FormControl>
+                <Button variant="contained" onClick={handlelogin}>
+                  Login
+                </Button>
+        </div>
+                
+    );
+};
+
+export default Login; 
