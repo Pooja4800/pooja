ed5e1324295a315d87f3ce69f6222be6bcd2174f
pooja4800 <poojam4800@gmail.com>
2022-08-17 19:39:26 +0530

    add the program

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
-import "./App.css"
-import Login from "./screens/dashboard"
+import React, {useState} from "react";
+import "./App.css";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 export default function App() {
+  const [array, setArray] = useState([ // hook
+    {name:"Seetharam", selected: false}, 
+    {name:"Don", selected: false}, 
+    {name:"Ganga", selected: false}, 
+    {name:"Murali", selected: false}, 
+    {name:"Arumdhathi", selected: false}
+  ]);
 
-    return(
-        <div className="App">
-            <Login/>
-        </div>
-    )
+  const handleCheck = (index) => {
+    let tempArray = array
+    tempArray[index].selected = !tempArray[index].selected
+    setArray([...tempArray])
+  }
+
+  return (
+    <div className="App">
+      <FormGroup>
+        {array.map((movie, index) => (
+          <FormControlLabel
+            key={index}
+            control={<Checkbox checked={movie.selected} onClick={() => handleCheck(index)}/>}
+            label={movie.name}
+          />
+        ))}
+      </FormGroup>
+    </div>
+  );
 }
